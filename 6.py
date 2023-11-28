5733ac3edece274979c68f1f2f2c7c72b17d9968
Pradeep-Deep14 <123350286+Pradeep-Deep14@users.noreply.github.com>
2023-11-28 19:46:50 +0530

    first commit

---
+++ 6.py
@@ -0,0 +1,8 @@
+def baz(lst):
+    for i in range(len(lst)):
+        for j in range(i+1,len(lst)):
+            if lst[i]==lst[j]:
+                return True
+    return False
+
+print(baz([1,2,3,4,5,5]))
