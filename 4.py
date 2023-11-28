486990600f420d0a80becb7c3eea9e894575626c
Pradeep-Deep14 <123350286+Pradeep-Deep14@users.noreply.github.com>
2023-11-28 19:30:03 +0530

    first commit

--- 4.py
+++ 4.py
@@ -4,3 +4,24 @@ data=[2,4,6,8]
 result=[modify(x) for x in data if x%4==0]
 
 print(len(result))
+
+
+#Answer is 2.
+
+#How?????
+#** Long story short, we are not printing result, just the length of result.
+
+#➡️def modify(n):    
+#            return n + 2
+
+#This defines a function named modify that takes a parameter n and returns the result of adding 2 to n. Essentially, it's a simple function that adds 2 to its input.
+
+#➡️result = [modify(x) for x in data if x % 4 == 0]
+
+#This line uses a list comprehension. It iterates over each element x in the list data, applies the modify function to x if x is divisible by 4 (i.e., x % 4 == 0), and creates a new list result with the modified values. In this case, it will only modify the values 4 and 8 because they are the ones divisible by 4.
+
+#So, after this line, result will be a list containing the modified values of 4 and 8. The modified value of 4 is 6 (4 + 2) and the modified value of 8 is 10 (8 + 2).
+
+#➡️print(len(result))
+
+#This line prints the length of the result list. In this case, the length will be 2 because there are two elements in the result list (6 and 10).
